3a358844bb2494f4730bdc950dabc64010a5be7f
TanyTany <avdtatyana@gmail.com>
2020-11-25 20:11:31 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 3500;
+        int forbonus = 20;
+        int totalbonus = price / forbonus;
+
+        System.out.println(totalbonus);
+
+
+    }
+}
